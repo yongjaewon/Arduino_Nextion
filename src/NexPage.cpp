5b6c8bffcea95d3b9c4632047f30031e4ef8b00d
Jyrki Berg <12322333+jyberg@users.noreply.github.com>
2020-07-26 16:52:46 +0300

    variable name corrected

--- src/NexPage.cpp
+++ src/NexPage.cpp
@@ -43,10 +43,11 @@ bool NexPage::setVisibleAll(bool visible)
 {
     String cmd = String("vis ");
     cmd += "255,";
-    if (flag)
+    if(visible)
     {
         cmd += "1";
-    } else
+    }
+    else
     {
         cmd += "0";
     }
